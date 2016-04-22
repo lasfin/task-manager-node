47329023005ddd7755f2af68464f0825c119b7e9
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-04-22 20:00:03 +0300

    edit client - connect frontend with backend

--- public/js/controllers/clients/editClient.js
+++ public/js/controllers/clients/editClient.js
@@ -7,19 +7,26 @@
         .controller('EditClient',
             ['$scope', '$stateParams', 'clientsFactory', 'ngDialog', 'Notification', 'helpers',
                 function ($scope, $stateParams, clientsFactory, ngDialog, Notification, helpers) {
-                    var savedClient = {};
-
                     clientsFactory.getById($stateParams.id)
                         .success((response) => {
                             $scope.client = response.client;
-                            savedClient = JSON.parse(JSON.stringify($scope.client));
                         })
                         .error(() => {
                             Notification.error('Can\'t find a task');
                         });
 
                     $scope.updateClient = function() {
-
+                        clientsFactory.update($scope.client._id, {
+                            _id: $scope.client._id,
+                            name: $scope.client.name,
+                            phone: $scope.client.phone,
+                            email: $scope.client.email,
+                            info: $scope.client.info
+                        }).then(() => {
+                            Notification.success('Successfully edited');
+                        }, () => {
+                            Notification.error('Something goes wrong');
+                        });
                     }
                 }])
 })();
